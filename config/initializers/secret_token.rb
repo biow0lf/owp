c97b21ddb59d3beb35ba442586f7dd97bc9f6039
Igor Zubkov <icesik@altlinux.org>
2012-11-30 16:25:58 +0200

    Add secret_token.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Owp::Application.config.secret_token = 'de4d2a8a66ed013966673a0633d8153f8290d018efd9e5c6d307291879c6b3b8d2b59e03d1466878e7c89c217836c618598ecfbb911b881d42732d95354dd774'
